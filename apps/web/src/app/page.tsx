4e2d0cd34cc14ce2eb37f75faf2d4f0157fa8929
AxeemHaider <axeemhaider@gmail.com>
2024-08-19 08:41:27 +0500

    setup storybook, tailwind css, nextjs app resloved #15, resolved #16, resolved #17, resolved #18

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -2,7 +2,7 @@ import { Card } from "@repo/ui/card";
 
 export default function Page(): JSX.Element {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between bg-gray-300 p-24">
       <Card href="#" title="Cart title">
         Card description
       </Card>
